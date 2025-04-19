1da13c4a73c8b5c6581d39cfbb0e5719fa3e1371
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-04-19 15:05:53 +0200

    updated newline for compiler

---
+++ chapter-01/exercise-01-07.c
@@ -0,0 +1,13 @@
+/* Exercise 1 - 7. 
+ * Write a program to print the value of EOF. 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int c = EOF;
+    
+    // The output is -1
+    printf("%d", c);
+}
