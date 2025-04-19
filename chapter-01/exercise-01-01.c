703434399b9dc2fe45e1298794cb432d49116444
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-04-19 15:01:06 +0200

    Added exercise

---
+++ chapter-01/exercise-01-01.c
@@ -0,0 +1,16 @@
+/* Exercise 1 - 1. 
+ * Run the ``hello, world`` program on your system. 
+ * Experiment with leaving
+ * out parts of the program, to see what error messages you get.
+ */
+
+// If any character from the printf statement is missing, the program won"t compile and we
+// get a compiler error. That applies for any missing character except characters within the double quotes. 
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world\n");
+
+}
